709f289fbf5d456c3bb12ab1a3578abd352a78b1
Damian V.N <dvnegrete@gmail.com>
2024-04-23 14:46:51 -0600

    Metodos agregados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: "Crear Proyecto",
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: "Algebra",
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: "modificar Estructura de Proyecto",
+      completed: false
+    },    
+  ]);
 
+  newTasksCtrol = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(1),
+    ]
+  });
+
+ 
+
+  changeHandler() {
+    if (this.newTasksCtrol.valid) {
+      const value = this.newTasksCtrol.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTasksCtrol.setValue('');        
+      }
+    }
+  }
+
+  addTask(title: string){
+    const newTask: Task = {
+      id: Date.now(),
+      title: title,
+      completed: false
+    }
+    this.tasks.update( (tasks) => [...tasks, newTask] );
+  }
+
+  deleteTask(index: number){
+    this.tasks.update( (tasks)=> tasks.filter( (task, posIndex)=> posIndex !== index));
+  }
+
+  updateTask(index: number){
+    this.tasks.update( (state)=> {
+      return state.map( (task, posIndex)=>{
+        if (posIndex === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    });
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,17 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-  title= "Hola"
-  tasks = [
-    "Priemra Tarea",
-    "Segunda TArea",
-    "Hola tu  TArea",
-  ]
+
+  title= "Hola";
+  
+  tasks = signal([
+    "Primera Tarea",
+    "Segunda Tarea",
+    "Hola tu  Tarea",
+    "Adios a tu  Tarea",
+  ]);
+  name= signal("dvnegrete");
+
+  disabled= false;
+
+  handlerClick() {
+    alert("ME diste!!")
+  }
+
+  handlerDoubleClick() {
+    alert("Me diste dos veces!!")
+  }
+
+  changeHandler(e: Event){
+    console.log((e));
+  }
+
+  keyDownHandler(e: KeyboardEvent){
+    const input = e.target as HTMLInputElement
+    console.log(input.value)
+  }
+
+  changeName(event: Event){
+    const input = event.target as HTMLInputElement;
+    this.name.set(input.value);
+  }
+
+  colorControl = new FormControl();
+
+  constructor(){
+    this.colorControl.valueChanges.subscribe(value=>{
+      console.log(value);
+      
+    })
+  }
+
 }
